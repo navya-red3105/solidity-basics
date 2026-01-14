5714b63fddc9c19740eefee70d5eb96cdcf1c387
navya-red3105 <palukuru_nr@cy.iitr.ac.in>
2026-01-14 10:36:55 +0530

    Add Greeter smart contract

---
+++ Greeter.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+contract Greeter {
+    string public greeting = "Hello";
+
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+}
